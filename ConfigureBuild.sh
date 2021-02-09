af266d6f4ecea70295588ecef63d9bb04430d47b
Jeremy Vial <jvial@witekio.com>
2021-02-09 10:38:49 +0100

    Reflect changes about config.cfg made into the meta-fullmetalupdate
    The goal of these changes is to give the possibility to use a real cloud.
    So it give more flexibility to configure how to connect to the different servers.

--- ConfigureBuild.sh
+++ ConfigureBuild.sh
@@ -1,14 +1,27 @@
+#!/bin/sh
+
+# This script is made to configure the build for the demo.
+# In this scenario, the Cloud part (Hawkbit and Ostree server) is played
+# by the fullmetalupdate-cloud-demo, have a look to:
+# https://github.com/FullMetalUpdate/fullmetalupdate-cloud-demo
+
+# Fullmetalupdate is build using a docker container. This container uses the
+# fmu_network. So it can access hawkbit and ostree servers thanks to there
+# hostname. It won't be the case of the target. Using the IP address of the
+# computer where the target can get the information from the Cloud is the
+# easiest way. So you can call this script like this:
+# FMU_DEMO_IP=<your IP address> ./ConfigureBuild.sh
+
 sed_escape() {
   sed -e 's/[]\/$*.^[]/\\&/g'
 }
 
-cfg_write() { # path, key, value
-  cfg_delete "$1" "$2"
-  echo "$2 = $3" >> "$1"
-}
-
 cfg_delete() { # path, key
   test -f "$1" && sed -i "/^$(echo $2 | sed_escape).*$/d" "$1"
+}
+
+cfg_replace() { # path, key , value
+  sed -i "s|^$2.*|$2 = $3|" "$1"
 }
 
 if [ -e ./config.cfg ]; then
@@ -25,12 +38,14 @@ fi
 
 cp ./config.cfg.sample ./config.cfg
 
-if [ -n "$FMU_CLOUD_HOSTNAME" ]; then
-  # Check if FMU_CLOUD_HOSTNAME env variable is defined: 
-  # that means that we are running inside buildbot worker container
-  cfg_write config.cfg server_host_name "$FMU_CLOUD_HOSTNAME".local
-  echo "Wrote server_host_name $FMU_CLOUD_HOSTNAME in config.cfg from FMU_CLOUD_HOSTNAME env variable"
+if [ -n "$FMU_DEMO_IP" ]; then
+    cfg_replace config.cfg hawkbit_url_host $FMU_DEMO_IP
+    cfg_replace config.cfg ostree_url_host $FMU_DEMO_IP
 else
-  cfg_write config.cfg server_host_name $(hostname).local
-  echo "Wrote server_host_name $(hostname) in config.cfg from hostname command"
-fi 
+    cfg_replace config.cfg hawkbit_url_host hawkbit
+    cfg_replace config.cfg ostree_url_host ostree
+fi
+
+cfg_replace config.cfg ostreepush_ssh_host ostree
+cfg_delete config.cfg ostree_url_path
+cfg_replace config.cfg ostreepush_ssh_path "/ostree/repo"
